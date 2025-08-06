cc1a3ca361fc60f5e4c931234e9ac0bf3b6196c5
seeun <seeun3139@sookmyung.ac.kr>
2025-08-06 21:07:07 +0900

    Setting/#1/경로 별칭 설정

--- spotal/vite.config.js
+++ spotal/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
+  },
 })
