1edc10b9950d21593a590be5bb7854d904331437
yexnsxo <yslove829@naver.com>
2025-08-11 15:17:06 +0900

    Settings/#1/tailwindcss 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
   },
