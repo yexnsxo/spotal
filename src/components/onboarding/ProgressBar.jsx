23a2f0bf264dfb2727f09350cb7a5d922538cd87
yexnsxo <yslove829@naver.com>
2025-08-12 19:08:01 +0900

    Refactor/#4/프로그래스 바 높이 단위 수정

--- src/components/onboarding/ProgressBar.jsx
+++ src/components/onboarding/ProgressBar.jsx
@@ -6,9 +6,9 @@ const ProgressBar = () => {
     <div className='flex ml-[5.9vw]'>
       <img className='mr-[3.44vw]' src={Return} alt='Return' />
       <div className='flex items-center justify-between gap-[2.56vw]'>
-        <div className='w-[25.64vw] h-[1.3vw] rounded-[12.8vw] bg-primary'></div>
-        <div className='w-[25.64vw] h-[1.3vw] rounded-[12.8vw] bg-grey-200'></div>
-        <div className='w-[25.64vw] h-[1.3vw] rounded-[12.8vw] bg-grey-200'></div>
+        <div className='w-[25.64vw] h-[1.3vh] rounded-[12.8vw] bg-primary'></div>
+        <div className='w-[25.64vw] h-[1.3vh] rounded-[12.8vw] bg-grey-200'></div>
+        <div className='w-[25.64vw] h-[1.3vh] rounded-[12.8vw] bg-grey-200'></div>
       </div>
     </div>
   )
