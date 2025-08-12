91d22dfd02eba896b04381496b19af4ad7c2e57b
yexnsxo <yslove829@naver.com>
2025-08-12 22:06:00 +0900

    Feat/#4/온보딩 페이지 UI 완성 및 다음 버튼 기능 적용

--- src/components/onboarding/ProgressBar.jsx
+++ src/components/onboarding/ProgressBar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Return from '../../assets/return.svg'
 
-const ProgressBar = () => {
+const ProgressBar = (step, totalSteps) => {
   return (
-    <div className='flex ml-[5.9vw]'>
-      <img className='mr-[3.44vw]' src={Return} alt='Return' />
+    <div className='flex ml-[5.9vw] mt-[25.48px]'>
+      <img className='mr-[3.44vw] w-[6.58px] h-[15.28px]' src={Return} alt='Return' />
       <div className='flex items-center justify-between gap-[2.56vw]'>
-        <div className='w-[25.64vw] h-[1.3vh] rounded-[12.8vw] bg-primary'></div>
-        <div className='w-[25.64vw] h-[1.3vh] rounded-[12.8vw] bg-grey-200'></div>
-        <div className='w-[25.64vw] h-[1.3vh] rounded-[12.8vw] bg-grey-200'></div>
+        <div className='w-[25.64vw] h-[0.59vh] rounded-[12.8vw] bg-primary'></div>
+        <div className='w-[25.64vw] h-[0.59vh] rounded-[12.8vw] bg-grey-200'></div>
+        <div className='w-[25.64vw] h-[0.59vh] rounded-[12.8vw] bg-grey-200'></div>
       </div>
     </div>
   )
--- src/pages/OnBoarding1.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const OnBoarding1 = () => {
-  return <div></div>
-}
-
-export default OnBoarding1
