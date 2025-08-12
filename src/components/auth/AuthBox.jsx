c5f23628178f8dd09ae6ce7f9f490161280a7e53
yexnsxo <yslove829@naver.com>
2025-08-13 02:43:06 +0900

    Style/#13/input 요소에 패딩값 적용

--- src/components/auth/AuthBox.jsx
+++ src/components/auth/AuthBox.jsx
@@ -4,7 +4,7 @@ const LoginBox = ({ label }) => {
   return (
     <div className='flex flex-col gap-[0.473vh]'>
       <p className='font-[SemiBold] text-grey-700 text-[17px]'>{label}</p>
-      <input className='w-[88.2vw] h-[44px] border-solid border-[2px] rounded-[7px] border-grey-100 focus:border-primary focus:outline-none' />
+      <input className='w-[88.2vw] h-[44px] border-solid border-[2px] rounded-[7px] border-grey-100 focus:border-primary focus:outline-none p-[0.5rem]' />
     </div>
   )
 }
