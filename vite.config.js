1b45f01316dedffcc714ed64836b2b4229b5b8e6
yexnsxo <yslove829@naver.com>
2025-08-13 01:35:42 +0900

    Chore/#13/svgr 패키지 설치

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
+import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), svgr()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
   },
