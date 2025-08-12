9b1c27e7eb70c17bbcaa69559f9d670784cd077a
yexnsxo <yslove829@naver.com>
2025-08-12 19:04:08 +0900

    Feat/#4/프로그래스 바 작성

---
+++ src/components/onboarding/ProgressBar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Return from '../../assets/return.svg'
+
+const ProgressBar = () => {
+  return (
+    <div className='flex ml-[5.9vw]'>
+      <img className='mr-[3.44vw]' src={Return} alt='Return' />
+      <div className='flex items-center justify-between gap-[2.56vw]'>
+        <div className='w-[25.64vw] h-[1.3vw] rounded-[12.8vw] bg-primary'></div>
+        <div className='w-[25.64vw] h-[1.3vw] rounded-[12.8vw] bg-grey-200'></div>
+        <div className='w-[25.64vw] h-[1.3vw] rounded-[12.8vw] bg-grey-200'></div>
+      </div>
+    </div>
+  )
+}
+
+export default ProgressBar
---
+++ src/pages/OnBoarding1.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const OnBoarding1 = () => {
+  return <div></div>
+}
+
+export default OnBoarding1
