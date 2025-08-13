98282f52082e674d7b0be4767355a9916ff576a9
yexnsxo <yslove829@naver.com>
2025-08-13 22:34:03 +0900

    Feat/#19/중복버튼 로직 추가

--- src/components/auth/AuthBox2.jsx
+++ src/components/auth/AuthBox2.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-const AuthBox2 = ({ label }) => {
+const AuthBox2 = ({ label, bg, onClick }) => {
   return (
     <div className='flex flex-col gap-[0.473vh]'>
       <p className='font-[SemiBold] text-grey-700 text-[17px]'>{label}</p>
       <div className='flex items-center justify-between w-[88.2vw] h-[5.213vh] min-h-[44px] border-solid border-[2px] rounded-[7px] border-grey-100 focus-within:border-primary p-[0.7rem]'>
         <input className='w-[67.7vw] focus:outline-none' />
-        <button className='w-[16.49vw] max-w-[63px] h-[2.844vh] min-h-[24px] bg-grey-100 rounded-[7px] text-grey-700 font-[SemiBold] text-[0.625rem]'>
+        <button
+          className={`w-[16.49vw] max-w-[63px] h-[2.844vh] min-h-[24px] ${bg} rounded-[7px] font-[SemiBold] text-[0.625rem]`}
+          onClick={onClick}
+          type='button'
+        >
           중복확인
         </button>
       </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Logo from '../assets/Logo.svg'
 import SignupImg from '../assets/Signup.svg'
 import AuthBox2 from '@/components/auth/AuthBox2.jsx'
@@ -6,6 +6,16 @@ import AuthBox from '@/components/auth/AuthBox.jsx'
 import AuthBtn from '@/components/auth/AuthBtn.jsx'
 
 const Signup = () => {
+  const [emailPressed, setEmailPressed] = useState(false)
+  const [nicknamPressed, setNicknamePressed] = useState(false)
+
+  const emailBtnBg = emailPressed
+    ? 'bg-[#FFF8EC] text-primary border border-[1px] border-primary'
+    : 'bg-grey-100 text-grey-700'
+  const nicknameBtnBg = nicknamPressed
+    ? 'bg-[#FFF8EC] text-primary border border-[1px] border-primary'
+    : 'bg-grey-100 text-grey-700'
+
   const postSignupRequest = () => {
     // axios
     //   .post(`${baseURL}/entries/`, {
@@ -21,15 +31,24 @@ const Signup = () => {
     //   })
   }
 
+  // const handleClick = () => {
+  //   postSignupRequest()
+  //   setPressed(true)
+  // }
+
   return (
     <div className='flex flex-col justify-center items-center justify-items-center'>
       <img className='mt-[11.492vh]' src={Logo} alt='Logo' />
       <img className='mt-[3.08vh]' src={SignupImg} alt='SignupImg' />
       <form>
         <div className='flex flex-col gap-[0.83vh] mt-0'>
-          <AuthBox2 label={'이메일'} />
+          <AuthBox2 label={'이메일'} bg={emailBtnBg} onClick={() => setEmailPressed((p) => !p)} />
           <AuthBox label={'비밀번호'} />
-          <AuthBox2 label={'닉네임'} />
+          <AuthBox2
+            label={'닉네임'}
+            bg={nicknameBtnBg}
+            onClick={() => setNicknamePressed((p) => !p)}
+          />
         </div>
         <div className='mt-[4.74vh]'>
           <AuthBtn bg={'bg-primary-200'} text={'회원가입 완료하기'} onClick={postSignupRequest} />
