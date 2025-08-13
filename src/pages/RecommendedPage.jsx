af1ddf48f47e7190aba776e96ee51b907ec552fa
seeun <seeun3139@sookmyung.ac.kr>
2025-08-13 13:47:49 +0900

    Feat/#17/추천된 장소 페이지 구현

---
+++ src/pages/RecommendedPage.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useLocation } from 'react-router-dom'
+import RecommendedPlaces from '@/components/recommend/RecommendedPlaces'
+
+const RecommendedPage = () => {
+  const location = useLocation()
+  const { placeData } = location.state || {}
+
+  {
+    /*넘어온 값을 바탕으로 로직 처리*/
+  }
+
+  return (
+    <div>
+      <div className='mt-[80px] w-[80%] mx-auto flex flex-col gap-4 mb-[80px]'>
+        {/*로직 처리 후, 추천 장소 컴포넌트 차례대로 추가*/}
+        <RecommendedPlaces
+          placeName={placeData.placeName}
+          status={placeData.status}
+          address={placeData.address}
+          summary={placeData.summary}
+          tags={placeData.tags}
+        />
+        <RecommendedPlaces
+          placeName={placeData.placeName}
+          status={placeData.status}
+          address={placeData.address}
+          summary={placeData.summary}
+          tags={placeData.tags}
+        />
+        <RecommendedPlaces
+          placeName={placeData.placeName}
+          status={placeData.status}
+          address={placeData.address}
+          summary={placeData.summary}
+          tags={placeData.tags}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default RecommendedPage
