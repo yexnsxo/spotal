9107ec0806350eee284f5b91a932aaf8af312b73
seeun <seeun3139@sookmyung.ac.kr>
2025-08-13 13:44:41 +0900

    Feat/#17/추천된 장소 인포 컴포넌트 구현

---
+++ src/components/recommend/RecommendedInfo.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import Close from '@/assets/Close.svg'
+
+const RecommendedInfo = ({ placeName, status, address, summary, tags = [], onClose }) => {
+  return (
+    <div className='fixed translate-y-[15%] left-1/2 -translate-x-1/2 w-[85%] max-w-[500px] bg-white rounded-[20px] shadow-lg p-6 z-[2000]'>
+      <div className='flex justify-end mr-2 mb-2'>
+        <img src={Close} onClick={onClose} className=''></img>
+      </div>
+
+      <img src='' className='bg-grey-100 w-[80vw] h-[15vh] rounded-[10px] mb-2'></img>
+
+      <div className='flex justify-between items-center mb-2 bg-primary-300 pt-3 pb-3 pl-5 pr-5 rounded-[10px]'>
+        <h2 className='m-0 font-bold text-[5vw]'>{placeName}</h2>
+        <span
+          className={`text-[3.5vw] px-3 py-1 rounded-full border font-semibold select-none ${
+            status === '폐업함'
+              ? 'bg-[#FDF2F8] text-[#BE195D] border-[#BE195D]'
+              : 'bg-green-600 text-white border-green-600'
+          }`}
+        >
+          {status}
+        </span>
+      </div>
+
+      <div className='bg-primary-300 pt-5 pb-3 pl-5 pr-5 rounded-[10px] mb-2'>
+        <p className='text-[4vw] text-black mb-2 whitespace-pre-wrap'>
+          <strong className='mr-1'>주소:</strong> {address}
+        </p>
+
+        <p className='text-[4vw] leading-relaxed text-black mb-2'>
+          <strong className='mr-1'>요약:</strong> {summary}
+        </p>
+
+        <div className='flex gap-1 flex-wrap mb-2'>
+          {tags.map((tag, idx) => (
+            <span
+              key={idx}
+              className='text-[3.2vw] text-gray-600 border border-[#ADADAD] px-2 py-1 rounded-[50px] select-none bg-[#FFFEFC]'
+            >
+              {tag}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default RecommendedInfo
