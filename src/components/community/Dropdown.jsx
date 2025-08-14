c82b64ef7c1a0278f345aa481e6433619fcb0169
yexnsxo <yslove829@naver.com>
2025-08-15 03:14:13 +0900

    Design/#28/드롭다운 메뉴 선택 상태에 배경색 추가

--- src/components/community/Dropdown.jsx
+++ src/components/community/Dropdown.jsx
@@ -7,6 +7,7 @@ const Dropdown = ({ label }) => {
   const areaList = ['전체', '이태원', '한남동', '후암동', '효창동', '용문동', '청파동', '해방촌']
   const [displayLabel, setDisplayLabel] = useState(label)
   const [filtered, setFiltered] = useState(false)
+  const [selected, setSelected] = useState('전체')
 
   const rootRef = useRef(null)
 
@@ -39,10 +40,12 @@ const Dropdown = ({ label }) => {
       </button>
 
       {open && (
-        <div className='flex flex-col bg-grey-100 rounded-[10px] w-[18.72vw] mt-[1px] font-[SemiBold] text-[0.625rem] text-grey-700 overflow-hidden absolute top-full left-0 z-50'>
+        <div className='flex flex-col bg-grey-100 rounded-[10px] w-[18.72vw] mt-[1px] font-[SemiBold] text-[0.625rem] text-grey-700 overflow-hidden absolute top-full left-0 z-40'>
           {options.map((opt) => (
             <button
-              className='hover:bg-primary-300 p-[5px]'
+              className={`hover:bg-primary-300 p-[5px] ${
+                selected === opt ? 'bg-primary-300' : 'bg-grey-100'
+              }`}
               key={opt}
               onClick={() => {
                 if (opt === '전체') {
@@ -53,6 +56,7 @@ const Dropdown = ({ label }) => {
                   setFiltered(true)
                 }
                 setOpen(false)
+                setSelected(opt)
               }}
             >
               {opt}
