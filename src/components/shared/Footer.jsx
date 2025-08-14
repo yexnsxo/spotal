923004817e64e46b5f6fbdd4f1100b99a6739617
yexnsxo <yslove829@naver.com>
2025-08-14 22:00:11 +0900

    Design/#26/헤더,푸터 고정(fixed) 적용

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -5,7 +5,7 @@ import Community from '../../assets/Community.svg?react'
 
 const Footer = ({ menu }) => {
   return (
-    <div className='flex items-center justify-between w-[100vw] pr-[7.82vw] pl-[7.82vw] h-[7.464vh]'>
+    <div className='fixed bottom-0 left-0 right-0 z-50 bg-white flex items-center justify-between w-[100vw] pr-[7.82vw] pl-[7.82vw] h-[7.464vh]'>
       <Home className={menu === 'home' ? 'text-primary' : 'text-grey-200'} />
       <Community className={menu === 'community' ? 'text-primary' : 'text-grey-200'} />
       <My className={menu === 'my' ? 'text-primary' : 'text-grey-200'} />
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 const Header = ({ label }) => {
   const navigate = useNavigate()
   return (
-    <div className='flex items-center relative w-[100vw] h-[7.11vh] border-b-[1px] border-b-grey-100 shadow-[0_2px_2px_#EFEFEF]'>
+    <div className='fixed top-0 left-0 right-0 z-50 bg-white flex items-center relative w-[100vw] h-[7.11vh] border-b-[1px] border-b-grey-100 shadow-[0_2px_2px_#EFEFEF]'>
       <img
         src={Return}
         alt='return'
