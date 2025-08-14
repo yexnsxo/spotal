dbe796f07f6300cceec0819b1e69b593beb4b788
yexnsxo <yslove829@naver.com>
2025-08-14 22:32:41 +0900

    Chore/#26/Footer props 변수명 수정

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -3,12 +3,12 @@ import My from '@/assets/My.svg?react'
 import Home from '@/assets/Home.svg?react'
 import Community from '@/assets/Community.svg?react'
 
-const Footer = ({ menu }) => {
+const Footer = ({ selectedMenu }) => {
   return (
     <div className='fixed bottom-0 left-0 right-0 z-50 bg-white flex items-center justify-between w-[100vw] pr-[7.82vw] pl-[7.82vw] h-[7.464vh]'>
-      <Home className={menu === 'home' ? 'text-primary' : 'text-grey-200'} />
-      <Community className={menu === 'community' ? 'text-primary' : 'text-grey-200'} />
-      <My className={menu === 'my' ? 'text-primary' : 'text-grey-200'} />
+      <Home className={selectedMenu === 'home' ? 'text-primary' : 'text-grey-200'} />
+      <Community className={selectedMenu === 'community' ? 'text-primary' : 'text-grey-200'} />
+      <My className={selectedMenu === 'my' ? 'text-primary' : 'text-grey-200'} />
     </div>
   )
 }
