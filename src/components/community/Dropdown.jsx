a48c1187ef85110476f8e04ebcae7ece10c3cc8a
yexnsxo <yslove829@naver.com>
2025-08-15 02:31:53 +0900

    Feat/#28/드롭다운 로직 구현

--- src/components/community/Dropdown.jsx
+++ src/components/community/Dropdown.jsx
@@ -5,27 +5,56 @@ const Dropdown = ({ label }) => {
   const [open, setOpen] = useState(false)
   const emotionList = ['전체', '정겨움', '편안함', '조용함', '활기참', '소박함', '세심함']
   const areaList = ['전체', '이태원', '한남동', '후암동', '효창동', '용문동', '청파동', '해방촌']
+  const [displayLabel, setDisplayLabel] = useState(label)
+  const [filtered, setFiltered] = useState(false)
+
+  const rootRef = useRef(null)
+
+  useEffect(() => {
+    if (!open) return
+    const onMouseDown = (e) => {
+      if (!rootRef.current?.contains(e.target)) {
+        setOpen(false)
+      }
+    }
+
+    document.addEventListener('mousedown', onMouseDown, { capture: false })
+    return () => document.removeEventListener('mousedown', onMouseDown, { capture: false })
+  }, [open])
 
   let options = []
   label === '감정' ? (options = emotionList) : label === '동네' ? (options = areaList) : ''
 
   return (
-    <div>
+    <div ref={rootRef}>
       <button
         type='button'
         onClick={() => {
           setOpen((o) => !o)
         }}
-        className='flex items-center justify-center w-[18.72vw] h-[3.08vh] bg-grey-100 cursor-pointer rounded-[5px] font-[SemiBold] text-[12px] text-grey-700'
+        className={`flex items-center justify-center w-[18.72vw] h-[3.08vh] bg-grey-100 cursor-pointer rounded-[5px] font-[SemiBold] text-[12px] text-grey-700 ${filtered ? 'bg-primary-300' : 'bg-grey-100'}`}
       >
-        {label}
+        {displayLabel}
         <img src={DropdownSign} />
       </button>
 
       {open && (
         <div className='flex flex-col bg-grey-100 rounded-[10px] mt-[1px] font-[SemiBold] text-[0.625rem] text-grey-700 overflow-hidden'>
           {options.map((opt) => (
-            <button className='hover:bg-primary-300 p-[5px]' key={opt}>
+            <button
+              className='hover:bg-primary-300 p-[5px]'
+              key={opt}
+              onClick={() => {
+                if (opt === '전체') {
+                  setDisplayLabel(label)
+                  setFiltered(false)
+                } else {
+                  setDisplayLabel(opt)
+                  setFiltered(true)
+                }
+                setOpen(false)
+              }}
+            >
               {opt}
             </button>
           ))}
