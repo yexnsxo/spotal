0b6716fa292c61f560b2f6d19874ff2e7b13ef57
yexnsxo <yslove829@naver.com>
2025-08-15 02:09:01 +0900

    Feat/#28/드롭다운 UI 구현

---
+++ src/components/community/Dropdown.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useRef } from 'react'
+import DropdownSign from '@/assets/DropdownSign.svg'
+
+const Dropdown = ({ label }) => {
+  const [open, setOpen] = useState(false)
+  const emotionList = ['전체', '정겨움', '편안함', '조용함', '활기참', '소박함', '세심함']
+  const areaList = ['전체', '이태원', '한남동', '후암동', '효창동', '용문동', '청파동', '해방촌']
+
+  let options = []
+  label === '감정' ? (options = emotionList) : label === '동네' ? (options = areaList) : ''
+
+  return (
+    <div>
+      <button
+        type='button'
+        onClick={() => {
+          setOpen((o) => !o)
+        }}
+        className='flex items-center justify-center w-[18.72vw] h-[3.08vh] bg-grey-100 cursor-pointer rounded-[5px] font-[SemiBold] text-[12px] text-grey-700'
+      >
+        {label}
+        <img src={DropdownSign} />
+      </button>
+
+      {open && (
+        <div className='flex flex-col bg-grey-100 rounded-[10px] mt-[1px] font-[SemiBold] text-[0.625rem] text-grey-700 overflow-hidden'>
+          {options.map((opt) => (
+            <button className='hover:bg-primary-300 p-[5px]' key={opt}>
+              {opt}
+            </button>
+          ))}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Dropdown
