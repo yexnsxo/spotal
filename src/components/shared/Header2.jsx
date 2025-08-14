ae435a4086b00ff2c9c0adc819d0a3bbe3288d3c
yexnsxo <yslove829@naver.com>
2025-08-15 02:54:03 +0900

    Feat/#28/Header z-index 오류 수정

--- src/components/shared/Header2.jsx
+++ src/components/shared/Header2.jsx
@@ -7,7 +7,7 @@ const Header2 = ({ label1, label2 }) => {
 
   return (
     <div>
-      <div className='fixed top-0 left-0 right-0 z-50 bg-white flex items-center relative w-[100vw] h-[7.11vh] border-b-[1px] border-b-grey-100 shadow-[0_2px_2px_#EFEFEF]'>
+      <div className='fixed top-0 left-0 right-0 z-50 bg-white flex items-center w-[100vw] h-[7.11vh] border-b-[1px] border-b-grey-100 shadow-[0_2px_2px_#EFEFEF]'>
         <img
           src={Return}
           alt='return'
