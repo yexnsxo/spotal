d5d0c7c40b30893b1005cb210a2402d8f97d4e78
seeun <seeun3139@sookmyung.ac.kr>
2025-08-15 14:26:07 +0900

    Feat/#30/Footer 기본값 설정

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -3,7 +3,7 @@ import My from '@/assets/My.svg?react'
 import Home from '@/assets/Home.svg?react'
 import Community from '@/assets/Community.svg?react'
 
-const Footer = ({ selectedMenu }) => {
+const Footer = ({ selectedMenu = '' }) => {
   return (
     <div className='fixed bottom-0 left-0 right-0 z-50 bg-white flex items-center justify-between w-[100vw] pr-[7.82vw] pl-[7.82vw] h-[7.464vh] rounded-t-[20px] shadow-[0_-1px_5px_rgba(0,0,0,0.1)]'>
       <Home className={selectedMenu === 'home' ? 'text-primary' : 'text-grey-200'} />
