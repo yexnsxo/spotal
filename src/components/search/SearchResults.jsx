c1e07cff9b026036b7c91cc8fe63b77fd85ca780
seeun <seeun3139@sookmyung.ac.kr>
2025-08-16 22:19:42 +0900

    Feat/#41/검색 배열 분리

--- src/components/search/SearchResults.jsx
+++ src/components/search/SearchResults.jsx
@@ -1,18 +1,34 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 
-const SearchResults = ({ results, goToMap }) => {
+const SearchResults = ({ keyword, goToMap }) => {
+  const [results, setResults] = useState([])
+
+  useEffect(() => {
+    if (!keyword.trim()) {
+      setResults([])
+      return
+    }
+
+    const ps = new kakao.maps.services.Places()
+    ps.keywordSearch(keyword, (data, status) => {
+      if (status === kakao.maps.services.Status.OK) {
+        setResults(data)
+      } else {
+        setResults([])
+      }
+    })
+  }, [keyword])
+
+  if (results.length === 0) return null
+
   return (
-    <div
-      className={`absolute top-[33vh] w-[85%] max-w-[500px] p-3 pt-2 pb-1 border-2 border-primary rounded-[20px] bg-white shadow-md z-20 overflow-y-auto max-h-[55vh] ${results.length === 0 ? 'hidden' : ''} scrollbar-hide`}
-    >
+    <div className='absolute top-[33vh] w-[85%] max-w-[500px] p-3 pt-2 pb-1 border-2 border-primary rounded-[20px] bg-white shadow-md z-20 overflow-y-auto max-h-[55vh] scrollbar-hide'>
       <ul>
         {results.map((place) => (
           <li
             key={place.id}
             className='cursor-pointer border-b-2 border-primary last:border-0 p-2 hover:bg-gray-100'
-            onClick={() => {
-              goToMap(place.place_name)
-            }}
+            onClick={() => goToMap(place.place_name)}
           >
             <p className='font-medium text-[4vw]'>{place.place_name}</p>
             <p className='text-[3.5vw] text-gray-500'>{place.address_name}</p>
