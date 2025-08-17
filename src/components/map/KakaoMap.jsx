929a696654ebf89f131ecfedbd1dd3c358844f3a
seeun <tpdms3139@naver.com>
2025-08-17 17:05:28 +0900

    Feat/#45/인포창 분리

--- src/components/map/KakaoMap.jsx
+++ src/components/map/KakaoMap.jsx
@@ -1,20 +1,47 @@
-import React from 'react'
-import { Map, MapMarker } from 'react-kakao-maps-sdk'
+import { useState } from 'react'
+import { Map } from 'react-kakao-maps-sdk'
 import Marker from './Marker'
+import InfoContainer from './InfoContainer'
 
 const KakaoMap = ({ center, markers }) => {
+  const [selectedMarker, setSelectedMarker] = useState(null)
+  const [isOpenMarker, setIsOpenMarker] = useState(false)
+  const [boundCenter, setCenter] = useState(center)
+
+  const handleMarkerClick = (marker) => {
+    if (selectedMarker && selectedMarker.placeName === marker.placeName) {
+      setIsOpenMarker(false)
+      setSelectedMarker(null)
+    } else {
+      setSelectedMarker(marker)
+      setIsOpenMarker(true)
+      setCenter({
+        lat: marker.lat,
+        lng: marker.lng,
+      })
+    }
+  }
+
   return (
-    <Map
-      center={center}
-      level={5}
-      style={{
-        width: '100vw',
-        height: '100vh',
-      }}
-    >
+    <Map center={boundCenter} level={5} style={{ width: '100vw', height: '100vh' }}>
       {markers.map((marker) => (
-        <Marker position={{ lat: marker.lat, lng: marker.lng }} placeName={marker.title}></Marker>
+        <Marker
+          key={marker.title}
+          position={{ lat: marker.lat, lng: marker.lng }}
+          placeName={marker.title}
+          onClick={handleMarkerClick}
+        />
       ))}
+
+      {isOpenMarker && (
+        <InfoContainer
+          placeName={selectedMarker.placeName}
+          status={selectedMarker.state}
+          address='서울 마포구 염리동 173-21'
+          summary='55년이 넘는 세월 동안 연탄불에 구운 납작 불고기로 사랑받았던 용산의 명소.'
+          tags={['정성스러움', '소박함', '따뜻함']}
+        />
+      )}
     </Map>
   )
 }
