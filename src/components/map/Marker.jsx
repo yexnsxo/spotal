90515de290445df171257dc2a5b10706a45cb7aa
seeun <tpdms3139@naver.com>
2025-08-18 22:41:06 +0900

    Feat/#53/클릭 마커 정보 반환

--- src/components/map/Marker.jsx
+++ src/components/map/Marker.jsx
@@ -2,13 +2,23 @@ import { MapMarker } from 'react-kakao-maps-sdk'
 import Marker1 from '@/assets/Marker1.svg'
 import Marker2 from '@/assets/Marker2.svg'
 
-const Marker = ({ position, placeName, state = '운영중', onClick }) => {
+const Marker = ({ position, placeName, address, state, summary, emotion, onClick }) => {
   const markerSrc = state === '운영중' ? Marker1 : Marker2
 
   return (
     <MapMarker
       position={position}
-      onClick={() => onClick({ lat: position.lat, lng: position.lng, placeName, state })}
+      onClick={() =>
+        onClick({
+          lat: position.lat,
+          lng: position.lng,
+          placeName,
+          address,
+          state,
+          summary,
+          emotion,
+        })
+      }
       image={{
         src: markerSrc,
         size: { width: 24, height: 30 },
