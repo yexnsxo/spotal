2d0d58ae18757718ed55a020c0f2dcfc12307def
yexnsxo <yslove829@naver.com>
2025-08-19 21:18:45 +0900

    Feat/#58/라우터 정리

--- src/router.jsx
+++ src/router.jsx
@@ -1,11 +1,56 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createBrowserRouter } from 'react-router-dom'
+import App from './App'
+import Mypage from './pages/Mypage.jsx'
+import Login from './pages/Login.jsx'
+import Signup from './pages/Signup.jsx'
+import OnBoarding from './pages/OnBoardingPage.jsx'
+import WritePage from './pages/WritePage.jsx'
+import EditPage from './pages/EditPage.jsx'
+import CommunityMyPostList from './pages/CommunityMyPostList'
+import CommunityPostList from './pages/CommunityPostList'
+import HomePage from './pages/HomePage.jsx'
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <App />,
+    path: '/mypage',
+    element: <Mypage />,
   },
-]);
+  {
+    path: '/',
+    element: <OnBoarding />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/signup',
+    element: <Signup />,
+  },
+  {
+    path: '/onboarding',
+    element: <OnBoarding />,
+  },
+  {
+    path: '/writepage',
+    element: <WritePage />,
+  },
+  {
+    path: '/editpage',
+    element: <EditPage />,
+  },
+  {
+    path: '/mypost',
+    element: <CommunityMyPostList />,
+  },
+  {
+    path: '/post',
+    element: <CommunityPostList />,
+  },
+  {
+    path: '/home',
+    element: <HomePage />,
+  },
+])
 
-export default router;
+export default router
