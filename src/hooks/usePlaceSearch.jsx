398879175e705198e54301b6f96e873992731de6
seeun <tpdms3139@naver.com>
2025-08-20 17:11:18 +0900

    Feat/#72/현재 위치 기준 찾기

--- src/hooks/usePlaceSearch.jsx
+++ src/hooks/usePlaceSearch.jsx
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import useGelocation from './useGelocation'
 
 const usePlaceSearch = () => {
   const [marker, setMarker] = useState([])
   const [status, setStatus] = useState('idle')
+  const current = useGelocation()
 
   const fetchMarker = async (keyword) => {
     if (!keyword.trim()) return { marker: [], status: 'idle' }
 
     setStatus('loading')
+
     try {
-      const res = await axios.get('http://127.0.0.1:8000/search/store/', {
-        params: { q: keyword, lat: 37.529128500148, lng: 126.9654885873 },
+      const res = await axios.get(`${import.meta.env.VITE_API_BASE_URL}/search/store/`, {
+        params: { q: keyword, lat: current.lat, lng: current.lng },
       })
       console.log(res.data)
       if (res.data && res.data.store) {
