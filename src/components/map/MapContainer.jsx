a3fb64e3a2e87281db27c7d8d8620e8a8f3818d0
seeun <tpdms3139@naver.com>
2025-08-20 16:40:22 +0900

    Feat/#67/Map 수정

--- src/components/map/MapContainer.jsx
+++ src/components/map/MapContainer.jsx
@@ -1,34 +1,7 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import KakaoMap from './KakaoMap'
-import usePlaceSearch from '@/hooks/usePlaceSearch'
 
 const MapContainer = ({ markers }) => {
-  useEffect(() => {
-    if (!navigator.geolocation) {
-      console.error('이 브라우저에서는 위치 정보가 지원되지 않습니다.')
-      return
-    }
-
-    const watchId = navigator.geolocation.watchPosition(
-      (pos) => {
-        const lat = pos.coords.latitude
-        const lng = pos.coords.longitude
-      },
-      (err) => {
-        console.error('위치 정보를 가져올 수 없습니다:', err)
-      },
-      {
-        enableHighAccuracy: true,
-        maximumAge: 0,
-        timeout: 5000,
-      },
-    )
-
-    return () => {
-      navigator.geolocation.clearWatch(watchId)
-    }
-  }, [])
-
   return (
     <div>
       <KakaoMap markers={markers} />
