7a94e32122657e1bd6fa38e746844a2bb126ef33
yexnsxo <yslove829@naver.com>
2025-08-20 18:49:30 +0900

    Feat/#70/게시물 없을 때 화면 추가

--- src/components/community/PostList.jsx
+++ src/components/community/PostList.jsx
@@ -1,13 +1,18 @@
 import Post from './Post.jsx'
+import NoResult from '@/assets/NoResult.svg'
 
 const PostList = ({ postData }) => {
   if (postData.length === 0) {
-    return <p>게시글이 없습니다</p>
+    return (
+      <div className='flex flex-col items-center justify-center mt-[25vh]'>
+        <img src={NoResult} className='w-[28vh] h-[28vh]' />
+        <p className='text-[18px] font-[Medium] text-gray-700'>올라온 게시물이 아직 없어요..</p>
+      </div>
+    )
   }
   return (
     <div className='flex flex-col gap-[3.91vh]'>
       {postData.map((post) => {
-        // console.log(post.images[0].image_url)
         return (
           <div key={post.memory_id}>
             <Post
