c3d5262bcdb92244030013a9d87e091ede1579b0
yexnsxo <yslove829@naver.com>
2025-08-22 03:32:56 +0900

    Design/#98/로그인 반응형 적용

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -33,26 +33,28 @@ const Login = () => {
   }
 
   return (
-    <div className='min-h-screen flex flex-col items-center justify-items-center bg-white'>
-      <img className='mt-[20vh]' src={Logo} />
-      <form className='flex flex-col gap-[1.184vh] mt-[5.094vh]'>
-        <AuthBox label='이메일' name='email' value={values.email} onChange={handleChange} />
-        <AuthBox
-          label={'비밀번호'}
-          name='password'
-          value={values.password}
-          onChange={handleChange}
-        />
-        <div className='mt-[4.74vh] flex flex-col justify-center items-center justify-items-center'>
-          <AuthBtn text={'로그인하기'} onClick={postLoginRequest} disabled={!isFilled} />
-          <p className='font-[Medium] font-[16px] text-primary mt-[1.184vh]'>
-            아직 계정이 없으신가요?
-            <Link to='/Signup' className='underline ml-[5px]'>
-              회원가입하기
-            </Link>
-          </p>
-        </div>
-      </form>
+    <div className='flex flex-col items-center justify-center'>
+      <div className='min-h-screen flex flex-col items-center justify-items-center bg-white w-full max-w-[768px]'>
+        <img className='mt-[20vh]' src={Logo} />
+        <form className='flex flex-col gap-[1.184vh] mt-[10vh]'>
+          <AuthBox label='이메일' name='email' value={values.email} onChange={handleChange} />
+          <AuthBox
+            label={'비밀번호'}
+            name='password'
+            value={values.password}
+            onChange={handleChange}
+          />
+          <div className='mt-[4.74vh] flex flex-col justify-center items-center justify-items-center'>
+            <AuthBtn text={'로그인하기'} onClick={postLoginRequest} disabled={!isFilled} />
+            <p className='font-[Medium] font-[16px] text-primary mt-[1.184vh]'>
+              아직 계정이 없으신가요?
+              <Link to='/Signup' className='underline ml-[5px]'>
+                회원가입하기
+              </Link>
+            </p>
+          </div>
+        </form>
+      </div>
     </div>
   )
 }
