8f426197689b5e653d24be2754efbf137f931cc4
seeun <tpdms3139@naver.com>
2025-08-22 22:46:16 +0900

    Feat/#91/추천 가게 이미지

--- src/components/recommend/RecommendedPlaces.jsx
+++ src/components/recommend/RecommendedPlaces.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react'
 import BookMark from '@/assets/BookMark.svg'
 import BookMark2 from '@/assets/BookMark2.svg'
 import RecommendedInfo from './RecommendedInfo'
+import NoResult from '@/assets/NoResult.svg'
 
-const RecommendedPlaces = ({ placeName, status, address, summary, tags = [] }) => {
+const RecommendedPlaces = ({ placeName, status, address, summary, tags = [], image = null }) => {
   const [isMarked, setIsMarked] = useState(false)
   const [isInfoOpen, setIsInfoOpen] = useState(false)
+  const [imgSrc, setImgSrc] = useState(image || NoResult)
 
   const handleOpenInfo = () => {
     setIsInfoOpen(true)
@@ -20,7 +22,14 @@ const RecommendedPlaces = ({ placeName, status, address, summary, tags = [] }) =
         className='max-w-[400px] bg-white rounded-[10px] shadow-[0_2px_7px_3px_rgba(0,0,0,0.1)] p-6 pb-4 cursor-pointer'
         onClick={handleOpenInfo}
       >
-        <img src='' className='bg-grey-100 w-[80vw] h-[150px] rounded-[10px] mb-2'></img>
+        <img
+          src={image}
+          className='bg-grey-100 w-[80vw] h-[150px] rounded-[10px] mb-2'
+          onError={(e) => {
+            e.currentTarget.src = NoResult
+            setImgSrc(NoResult)
+          }}
+        ></img>
         <div className='flex justify-between items-center mb-2 pt-1 pl-1 rounded-[10px]'>
           <h2 className='m-0 font-bold text-xl'>{placeName}</h2>
           <img
@@ -49,6 +58,7 @@ const RecommendedPlaces = ({ placeName, status, address, summary, tags = [] }) =
           address={address}
           summary={summary}
           tags={tags}
+          image={imgSrc}
           onClose={handleCloseInfo}
         />
       )}
