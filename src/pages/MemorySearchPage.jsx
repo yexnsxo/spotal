b3bad158ef2dd4c550feb64bff8542a02388083d
seeun <tpdms3139@naver.com>
2025-08-23 22:01:03 +0900

    Feat/#129/감정 동네 전달

--- src/pages/MemorySearchPage.jsx
+++ src/pages/MemorySearchPage.jsx
@@ -1,18 +1,25 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import StepSelector from '@/components/memory/StepSelector'
+import useRecommend from '@/hooks/useRecommend'
+import Loading from '@/components/shared/Loading'
 
 const MemorySearchPage = () => {
   const navigate = useNavigate()
   const [step, setStep] = useState(0)
   const [emotion, setEmotion] = useState([])
   const [location, setLocation] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const { fetchRecommend2 } = useRecommend()
 
-  const handleNext = () => {
+  const handleNext = async () => {
     if (step === 0) setStep(1)
     else {
-      console.log('최종 데이터:', { emotion, location })
-      // navigate('/recommended')
+      setIsLoading(true)
+      const recommend = await fetchRecommend2(location, emotion)
+      navigate('/recommended', {
+        state: { placeData: recommend },
+      })
     }
   }
 
@@ -21,17 +28,20 @@ const MemorySearchPage = () => {
     else setStep(step - 1)
   }
 
-  const selected = step === 0 ? emotion : location
-  const setSelected = step === 0 ? setEmotion : setLocation
+  const selected = step === 0 ? location : emotion
+  const setSelected = step === 0 ? setLocation : setEmotion
 
   return (
-    <StepSelector
-      step={step}
-      selected={selected}
-      onSelect={setSelected}
-      onNext={handleNext}
-      onPrev={handlePrev}
-    />
+    <>
+      {isLoading && <Loading />}
+      <StepSelector
+        step={step}
+        selected={selected}
+        onSelect={setSelected}
+        onNext={handleNext}
+        onPrev={handlePrev}
+      />
+    </>
   )
 }
 
