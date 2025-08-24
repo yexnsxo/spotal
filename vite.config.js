0842aebd88d1dee157e54adf23ac1281f6b94f81
yexnsxo <yslove829@naver.com>
2025-08-24 19:31:07 +0900

    Chore/vite.config.js 수정

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/',
   plugins: [svgr(), react(), tailwindcss()],
   resolve: {
     alias: [{ find: '@', replacement: '/src' }], // '@'를 '/src' 폴더로 대체
