1346ef86b797a320561a694273b135690b89225f
seeun <tpdms3139@naver.com>
2025-08-24 22:32:31 +0900

    Feat/#dev/이미지, 버튼

--- src/components/search/SearchNoResults.jsx
+++ src/components/search/SearchNoResults.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import Close from '@/assets/Close.svg'
 import NoResult from '@/assets/NoResult.svg'
+import { useNavigate } from 'react-router-dom'
 
 const SearchNoResults = ({ onClose }) => {
+  const navigate = useNavigate()
   return (
     <div className='fixed inset-0 bg-[rgba(173,173,173,0.5)] z-[1000] flex items-center justify-center'>
       <div className='bg-white rounded-[20px] shadow-lg p-6 w-[85%] max-w-[400px] flex flex-col items-center'>
@@ -14,10 +16,16 @@ const SearchNoResults = ({ onClose }) => {
 
         <img src={NoResult} alt='character' />
 
-        <button className='w-[90%] h-[7vh] py-2.5 mt-[3vh] mb-[1vh] rounded-[10px] border border-primary-200 bg-white text-grey-700 text-m cursor-pointer select-none transition-colors duration-300 hover:bg-primary hover:text-white'>
+        <button
+          onClick={() => navigate('/post')}
+          className='w-[90%] h-[7vh] py-2.5 mt-[3vh] mb-[1vh] rounded-[10px] border border-primary-200 bg-white text-grey-700 text-m cursor-pointer select-none transition-colors duration-300 hover:bg-primary hover:text-white'
+        >
           커뮤니티에 물어보기
         </button>
-        <button className='w-[90%] h-[7vh] py-2.5 rounded-[10px] border border-primary-200 bg-white text-grey-700 text-m cursor-pointer select-none transition-colors duration-300 hover:bg-primary hover:text-white'>
+        <button
+          onClick={() => navigate('/post')}
+          className='w-[90%] h-[7vh] py-2.5 rounded-[10px] border border-primary-200 bg-white text-grey-700 text-m cursor-pointer select-none transition-colors duration-300 hover:bg-primary hover:text-white'
+        >
           가게 제보하기
         </button>
       </div>
