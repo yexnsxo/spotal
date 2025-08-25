6f9e3bc28c197997fc49fb7cb7d0c40845cbfc97
yexnsxo <yslove829@naver.com>
2025-08-25 17:31:41 +0900

    Design/#148/버튼 내 이미지 추가

--- src/components/community/PostMenu.jsx
+++ src/components/community/PostMenu.jsx
@@ -4,6 +4,7 @@ import { useNavigate, useParams } from 'react-router-dom'
 import useOutsideClick from '@/hooks/useOutsideClick'
 import { baseURL } from '@/pages/Signup'
 import axios from 'axios'
+import MenuSign from '@/assets/MenuSign.svg?react'
 
 const PostMenu = ({ memory_id }) => {
   const [open, setOpen] = useState(false)
@@ -38,18 +39,20 @@ const PostMenu = ({ memory_id }) => {
         <img src={PostMenuSvg} alt='메뉴 열기' />
       </button>
       {open && (
-        <div className='flex flex-col bg-white rounded-[10px] w-[18.72vw] mt-[1px] font-[SemiBold] text-[0.625rem] text-grey-700 overflow-hidden absolute top-full right-[-10px] z-40 shadow-[0_4px_4px_rgba(0,0,0,0.1)]'>
+        <div className='flex flex-col bg-white rounded-[10px] w-[18.72vw] max-w-[100px] mt-[1px] font-[SemiBold] text-[0.625rem] text-grey-700 overflow-hidden absolute top-full right-[-10px] z-40 shadow-[0_4px_4px_rgba(0,0,0,0.1)]'>
           <button
-            className='py-[0.5rem] hover:bg-primary-300 cursor-pointer border-b-[1px] border-gray-100 active:bg-primary-300'
+            className='flex justify-center items-center gap-[10px] py-[0.5rem] hover:bg-primary-300 cursor-pointer border-b-[1px] border-gray-100 active:bg-primary-300'
             onClick={() => navigate(`/editpage/${memory_id}`)}
           >
             수정하기
+            <MenuSign />
           </button>
           <button
-            className='py-[0.5rem] hover:bg-primary-300 cursor-pointer active:bg-primary-300'
+            className='flex justify-center items-center gap-[10px] py-[0.5rem] hover:bg-primary-300 cursor-pointer active:bg-primary-300'
             onClick={deletePost}
           >
             삭제하기
+            <MenuSign />
           </button>
         </div>
       )}
