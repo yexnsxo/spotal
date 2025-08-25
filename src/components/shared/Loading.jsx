7fa2a35a1c6ecdb0648547528e89c86000a312d0
seeun <tpdms3139@naver.com>
2025-08-25 17:39:06 +0900

    Design/#147/로딩 화면 개선

--- src/components/shared/Loading.jsx
+++ src/components/shared/Loading.jsx
@@ -5,10 +5,10 @@ import { BeatLoader } from 'react-spinners'
 const Loading = () => {
   return (
     <div className='w-full max-w-[768px] mx-auto h-screen flex flex-col justify-center items-center bg-white absolute left-1/2 -translate-x-1/2 z-2000'>
-      <h1 className='font-[Bold] text-grey-700 text-xl mb-2'>잠시만 기다려주세요</h1>
-      <h1 className='font-[Bold] text-grey-700 text-xl mb-15'>로딩 중입니다!</h1>
+      <h1 className='font-[Bold] text-grey-700 text-xl mb-2 mt-[5vh]'>잠시만 기다려주세요</h1>
+      <h1 className='font-[Bold] text-grey-700 text-xl mb-20'>로딩 중입니다!</h1>
       <BeatLoader color='#ffba42' />
-      <div className='relative flex justify-center items-center mt-[15vh]'>
+      <div className='relative flex justify-center items-center mt-[10vh]'>
         {/* 블러 배경 */}
         <div className='absolute w-[100%] h-[100%] bg-primary-200 blur-[9rem] rounded-full pointer-events-none -z-10 animate-pulse bg-gradient-to-b from-primary-200 to-primary' />
         {/* 캐릭터 이미지 */}
